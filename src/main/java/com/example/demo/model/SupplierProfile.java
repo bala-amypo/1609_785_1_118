279af1d1ac791992ea0c032496df862d356735b6
bala-amypo <bala@amypo.in>
2025-12-18 15:04:06 +0000

    User commit

--- src/main/java/com/example/demo/model/SupplierProfile.java
+++ src/main/java/com/example/demo/model/SupplierProfile.java
@@ -1,6 +1,60 @@
-package com.example.demo.model;
-import jakarta.peristence.Entity;
+package com.example.demo.entity;
+import java.util.Date;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 @Entity
-public class SupplierProfile{
-    
+public class StudentEntity{
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+    private String supplierCode;
+    private String supllierName;
+    private String email;
+    private String phone;
+    private String phone;
+    public void setId(Integer id){
+        this.id=id;
+    }
+    public void setUsername(String username){
+        this.username=username;
+    }
+    public void setEmail(String email){
+        this.email=email;
+    }
+     public void setPassword(String password){
+        this.password=password;
+    }
+    public void setCreatedAt(Date createdAt){
+        this.createdAt=createdAt;
+    }
+    public Integer getId(){
+        return id;
+    }
+    public String getUsername(){
+        return username;
+    }
+    public String getEmail(){
+        return email;
+    }
+    public String getPassword(){
+        return password;
+    }
+   
+    public Date getCreatedAt(){
+        return createdAt;
+    }
+    public StudentEntity(Integer id,String username,String email,String password,Date createdAt){
+        this.username=username;
+        this.email=email;
+        this.password=password;
+        this.createdAt=createdAt;
+    }
+    public StudentEntity(){
+
+    }
+
+
+
 }
