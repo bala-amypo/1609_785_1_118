1d7091632bc89016d1f9c73f6e23ae6fb9f94825
bala-amypo <bala@amypo.in>
2025-12-18 15:37:07 +0000

    User commit

--- src/main/java/com/example/demo/model/SupplierProfile.java
+++ src/main/java/com/example/demo/model/SupplierProfile.java
@@ -2,6 +2,7 @@ package com.example.demo.model;
 import java.util.Date;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Column;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 @Entity
