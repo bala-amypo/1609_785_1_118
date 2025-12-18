cfad0f0b9b8a897de7226a15429a72905a3e4010
bala-amypo <bala@amypo.in>
2025-12-18 17:58:05 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/Impl/SupplierProfileServiceImpl.java
@@ -0,0 +1,36 @@
+
+package com.example.demo.service.impl;
+
+import com.example.demo.model.PurchaseOrderRecord;
+import com.example.demo.repository.PurchaseOrderRecordRepository;
+import com.example.demo.service.PurchaseOrderService;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class PurchaseOrderServiceImpl implements PurchaseOrderService {
+
+    private final PurchaseOrderRecordRepository repo;
+
+    public PurchaseOrderServiceImpl(PurchaseOrderRecordRepository repo) {
+        this.repo = repo;
+    }
+
+    public PurchaseOrderRecord createPurchaseOrder(PurchaseOrderRecord po) {
+        return repo.save(po);
+    }
+
+    public List<PurchaseOrderRecord> getPOsBySupplier(Long supplierId) {
+        return repo.findBySupplierId(supplierId);
+    }
+
+    public PurchaseOrderRecord getPOById(Long id) {
+        return repo.findById(id).orElseThrow();
+    }
+
+    public List<PurchaseOrderRecord> getAllPurchaseOrders() {
+        return repo.findAll();
+    }
+}
+
+
