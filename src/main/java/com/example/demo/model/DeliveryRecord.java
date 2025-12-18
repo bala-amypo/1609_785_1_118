954d41c7b29e4d7cef301ce7f72715744a6b091c
bala-amypo <bala@amypo.in>
2025-12-18 16:10:06 +0000

    User commit

--- src/main/java/com/example/demo/model/DeliveryRecord.java
+++ src/main/java/com/example/demo/model/DeliveryRecord.java
@@ -11,18 +11,15 @@ public class PurchaseOrderRecord{
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Long id;
-    private String poNumber;
-    private Long supplierId;
-    private String itemDescription;
-    @Min(1)
-    private Integer quantity;
-    private LocalDate promisedDeliveryDate;
-    private LocalDate issuedDate;
+    private String pold;
+    private LocalDate actualDeliveryDate;
+    private Integer deliveredQuantity;
+    private String note;
     public void setId(Long id){
         this.id=id;
     }
-    public void setPoNumber(String poNumber){
-        this.poNumber=poNumber;
+    public void setPold(String pold){
+        this.pold=pold;
     }
     public void setSupplierId(Long supplierId){
         this.supplierId=supplierId;
@@ -42,8 +39,8 @@ public class PurchaseOrderRecord{
     public Long getId(){
         return id;
     }
-    public String getPoNumber(){
-        return poNumber;
+    public String getPold(){
+        return pold;
     }
      public Long getSupplierId(){
         return supplierId;
@@ -62,11 +59,10 @@ public class PurchaseOrderRecord{
     public LocalDate getIssuedDate(){
         return issuedDate;
     }
-    public PurchaseOrderRecord(Long id,String poNumber,Long supplierId,String itemDescription,Integer quantity,LocalDate promisedDeliveryDate,LocalDate issuedDate){
+    public PurchaseOrderRecord(Long id,String pold,Long supplierId,String itemDescription,Integer quantity,LocalDate promisedDeliveryDate,LocalDate issuedDate){
         this.id=id;
-        this.poNumber=poNumber;
+        this.pold=poNumber;
         this.supplierId=supplierId;
-        this.itemDescription=itemDescription;
         this.quantity=quantity;
         this.promisedDeliveryDate=promisedDeliveryDate;
         this.issuedDate=issuedDate;
