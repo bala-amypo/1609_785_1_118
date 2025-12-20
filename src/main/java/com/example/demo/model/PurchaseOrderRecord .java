f1f14c4beffa3bdac2b7460bc039258906de22c2
bala-amypo <bala@amypo.in>
2025-12-20 11:26:57 +0000

    User commit

--- src/main/java/com/example/demo/model/PurchaseOrderRecord .java
+++ src/main/java/com/example/demo/model/PurchaseOrderRecord .java
@@ -8,23 +8,23 @@ import jakarta.persistence.Column;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 
 @Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class SupplierProfile {
+public class PurchaseOrderRecord {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     
     @Column(unique = true, nullable = false)
-    private String supplierCode;
+    private String poNumber;
     
-    private String supplierName;
-    private String email;
-    private String phone;
-    private Boolean active;
-    private LocalDateTime createdAt = LocalDateTime.now();
+    private Long supplierId;
+    private String itemDescription;
+    private Integer quantity;
+    private LocalDate promisedDeliveryDate;
+    private LocalDate issuedDate;
 }
