06860fc9a2fca4955c35a613786ebe88c3dff6fa
bala-amypo <bala@amypo.in>
2025-12-20 13:28:57 +0000

    User commit

--- src/main/java/com/example/demo/model/SupplierProfile.java
+++ src/main/java/com/example/demo/model/SupplierProfile.java
@@ -11,6 +11,7 @@ public class SupplierProfile {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(unique = true, nullable = false)
     private String supplierCode;
     private String supplierName;
     private Boolean active;
@@ -21,11 +22,25 @@ public class SupplierProfile {
     public Long getId() { 
         return id; 
         }
-    public void setId(Long id) { this.id = id; }
-    public String getSupplierCode() { return supplierCode; }
-    public void setSupplierCode(String code) { this.supplierCode = code; }
-    public String getSupplierName() { return supplierName; }
-    public void setSupplierName(String name) { this.supplierName = name; }
-    public Boolean getActive() { return active; }
-    public void setActive(Boolean active) { this.active = active; }
+    public void setId(Long id) { 
+        this.id = id; 
+        }
+    public String getSupplierCode() { 
+        return supplierCode; 
+        }
+    public void setSupplierCode(String code) { 
+        this.supplierCode = code; 
+        }
+    public String getSupplierName() { 
+        return supplierName; 
+        }
+    public void setSupplierName(String name) { 
+        this.supplierName = name; 
+        }
+    public Boolean getActive() { 
+        return active; 
+        }
+    public void setActive(Boolean active) { 
+        this.active = active; 
+        }
 }
