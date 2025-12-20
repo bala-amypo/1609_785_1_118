8a8fd424580a4caf41b2cfbb1eeee5f8a73052f9
bala-amypo <bala@amypo.in>
2025-12-20 10:01:47 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/Impl/SupplierProfileServiceImpl.java
@@ -0,0 +1,45 @@
+package com.example.demo.service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import org.springframework.stereotype.Service;
+import com.example.demo.model.SupplierProfile;
+import com.example.demo.repository.SupplierProfileRepository;
+
+@Service
+public class SupplierProfileImpl implements SupplierProfileService {
+
+    private SupplierProfileRepository repository;
+
+    public SupplierProfileImpl(SupplierProfileRepository repository) {
+        this.repository = repository;
+    }
+
+    public SupplierProfile saveSupplier(SupplierProfile supplier) {
+        supplier.setCreatedAt(LocalDateTime.now());
+        return repository.save(supplier);
+    }
+
+    public SupplierProfile getSupplierById(Long id) {
+        return repository.findById(id).orElse(null);
+    }
+
+    public List<SupplierProfile> getAllSuppliers() {
+        return repository.findAll();
+    }
+
+    public SupplierProfile updateSupplier(Long id, SupplierProfile supplier) {
+        SupplierProfile existing = repository.findById(id).orElse(null);
+        if (existing != null) {
+            existing.setSupplierCode(supplier.getSupplierCode());
+            existing.setName(supplier.getName());
+            existing.setStatus(supplier.getStatus());
+            return repository.save(existing);
+        }
+        return null;
+    }
+
+    public void deleteSupplier(Long id) {
+        repository.deleteById(id);
+    }
+}
