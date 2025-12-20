5af7976e6c983f1983e5ed2e4f63cd54ee18ce65
bala-amypo <bala@amypo.in>
2025-12-20 10:23:57 +0000

    User commit

--- src/main/java/com/example/demo/controller/DelayScoreController .java
+++ src/main/java/com/example/demo/controller/DelayScoreController .java
@@ -2,7 +2,6 @@ package com.example.demo.controller;
 
 import java.time.LocalDateTime;
 import java.util.List;
-
 import org.springframework.web.bind.annotation.*;
 
 import com.example.demo.model.DelayScoreRecord;
