4aae950532ad751686ad40fc0274a7ee635ac649
bala-amypo <bala@amypo.in>
2025-12-21 17:30:55 +0000

    User commit

--- src/main/java/com/example/demo/model/DeliveryRecord.java
+++ src/main/java/com/example/demo/model/DeliveryRecord.java
@@ -5,6 +5,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.persistence.Min;
 import jakarta.persistence.PrePersist;
 import java.time.LocalDateTime;
 
