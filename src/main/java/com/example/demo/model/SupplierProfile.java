8d0f5372cdea2620037e977fc99fb9ef0aaca419
bala-amypo <bala@amypo.in>
2025-12-21 14:59:29 +0000

    User commit

--- src/main/java/com/example/demo/model/SupplierProfile.java
+++ src/main/java/com/example/demo/model/SupplierProfile.java
@@ -10,56 +10,40 @@ import java.time.LocalDateTime;
 
 @Entity
 public class SupplierProfile {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     @Column(unique = true, nullable = false)
     private String supplierCode;
-
     @Column(nullable = false)
     private String supplierName;
-
     private Boolean active = true;
-
     @Column(nullable = false, updatable = false)
     private LocalDateTime createdAt;
-
     public SupplierProfile() {
     }
-
-    // ✅ Runs before insert
     @PrePersist
     protected void onCreate() {
         this.createdAt = LocalDateTime.now();
     }
-
-    // Getters and Setters
     public Long getId() {
         return id;
     }
-
     public void setId(Long id) {
         this.id = id;
     }
-
     public String getSupplierCode() {
         return supplierCode;
     }
-
     public void setSupplierCode(String supplierCode) {
         this.supplierCode = supplierCode;
     }
-
     public String getSupplierName() {
         return supplierName;
     }
-
     public void setSupplierName(String supplierName) {
         this.supplierName = supplierName;
     }
-
     public Boolean getActive() {
         return active;
     }
