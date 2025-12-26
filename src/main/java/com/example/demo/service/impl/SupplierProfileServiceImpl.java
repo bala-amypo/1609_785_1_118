9df23523a02c36d1579772a2f5258ab0c9ba192f
bala-amypo <bala@amypo.in>
2025-12-26 04:38:09 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/SupplierProfileServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/SupplierProfileServiceImpl.java
@@ -1,3 +1,15 @@
+package com.example.demo.service.impl;
+
+import com.example.demo.model.SupplierProfile;
+import com.example.demo.repository.SupplierProfileRepository;
+import com.example.demo.service.SupplierProfileService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class SupplierProfileServiceImpl implements SupplierProfileService {
 
