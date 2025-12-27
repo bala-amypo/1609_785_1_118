4b08115a7362816d5c8d8e43ae4600b8313682a6
bala-amypo <bala@amypo.in>
2025-12-27 10:37:47 +0000

    User commit

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -1,5 +1,4 @@
 package com.example.demo.config;
-
 import com.example.demo.security.JwtAuthenticationFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -11,8 +10,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;/
 @Configuration
 @EnableMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
